d9bf35612459bd37d0c2685af92bc0897c4a7139
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2025-03-22 10:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort(function(a, b){return a-b});
+	let count=0;
+	let count=arr[i]+arr[j];
+	for(let i=2;i<arr.length;i++)
+		{
+			let tempc=count+arr[i];
+			count=count+tempc;
+		}
+	
+	
+	return arr;
   
 }
 
